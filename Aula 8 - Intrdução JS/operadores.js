f24dd2ff8cf576054553c8863830b63d52b5da28
Lucasfusco08 <lucasgabriel.fusco@gmail.com>
2025-08-07 15:47:44 -0300

    .

---
+++ Aula 8 - Intrdução JS/operadores.js
@@ -0,0 +1,33 @@
+let numero1 = 5
+let numero2 = 5
+
+//Operador soma
+let soma = numero1 + numero2
+console.log("A soma foi igual a " + soma);
+
+//Operador subtração
+let subtração = numero2 - numero1
+console.log("A subtração foi igual a " + subtração)
+
+//Operador multiplicação
+let multiplicação = numero1 * numero2
+console.log ("A multiplicação foi igual a " + multiplicação)
+
+//Operador divisão
+let divisão = numero1 / numero2
+console.log("A divisão foi igual a " + divisão)
+
+//Operador módulo
+numero2 = 2
+let modulo = numero1 % numero2
+console.log("o resto foi igual a " + modulo)
+
+//Poência - Math.po()
+let base = 5
+let expoente = 2
+let potencia = Math.pow(base , expoente)
+console.log ("A potencia é igual a " + potencia)
+
+//Radiação - Math.sqrt()
+let radiação = Math.sqrt(potencia)
+console.log("A raiz quadrada é igual a " + radiação)
---
+++ Aula 8 - Intrdução JS/script.js
@@ -0,0 +1,12 @@
+// Dado do tipo - String
+console.log("hello  World")
+console.log('olá mundo')
+console.log("2" + "2")
+
+//dado do tipo - Number
+console.log(2 + 2)//somar
+console.log("f")//concatenar
+
+//dado do tipo - Boolean
+console.log(true)
+console.log(false)
