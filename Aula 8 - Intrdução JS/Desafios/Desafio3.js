ab3ba24c9c567103b8a40d0f0a30989385f4a18b
Lucasfusco08 <lucasgabriel.fusco@gmail.com>
2025-08-07 17:00:41 -0300

    .

---
+++ Aula 8 - Intrdução JS/Desafios/Desafio3.js
@@ -0,0 +1,6 @@
+let nota1 = 5
+let nota2 = 10
+
+let media = (nota1 + nota2)
+
+console.log("A sua média foi de " + media)
---
+++ Aula 8 - Intrdução JS/Desafios/desafio2.js
@@ -0,0 +1,11 @@
+let numero = 8
+let dobro = numero * 2
+let triplo = numero * 3
+let raiz = Math.sqrt(numero)
+let potencia = Math.pow(numero, 0.5)
+
+console.log("O número informaivo foi " + numero)
+console.log("O dobro é igual a " + dobro)
+console.log("O triplo é igual a " + triplo)
+console.log("A raiz quadrada é igual a " + raiz)
+console.log("A raiz quadrada é igual a " + potencia)
