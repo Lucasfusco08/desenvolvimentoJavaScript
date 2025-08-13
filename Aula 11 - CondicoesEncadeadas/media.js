1c07c6dd6da27937195bd27a867fb50db9e384e5
Lucasfusco08 <lucasgabriel.fusco@gmail.com>
2025-08-13 16:53:39 -0300

    .

--- Aula 11 - CondicoesEncadeadas/media.js
+++ Aula 11 - CondicoesEncadeadas/media.js
@@ -5,8 +5,17 @@ let media = (nota1 + nota2) / 2
 
 if (media >= 70){
     console.log("APROVADO");
+    
 } else if(media >= 50) {
     console.log("RECUPERAÇÃO")
+    let recuperaao = 90
+    letmediaFinal = (media + recuperacao) / 3
+
+    if(mediaFinal >= 70){
+        console.log("APROVADO")
+    }else{
+        console.log("REPROVADO")
+    }
 }
 
 else {
